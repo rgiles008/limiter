0a24607141e011c1d467ddf4aea66f674e8881ff
Robert Giles <robert.giles888@gmail.com>
2025-11-16 14:52:27 -0500

    Initial commit

---
+++ lib/limiter/application.ex
@@ -0,0 +1,20 @@
+defmodule Limiter.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Limiter.Worker.start_link(arg)
+      # {Limiter.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Limiter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
